62fdbf04a4922f26d18b536afac33184f9514ad6
Diego M Riano-Pachon <diriano@gmail.com>
2016-12-29 19:07:40 -0200

    Moving the R code into an independent R script. Fixing the order of the categories so that they always appear with the same colors. Fixing the set of colours used. Set theme to BW
    Former-commit-id: d0e92962a733a23d2d75d26443ed39bcef2694d3

---
+++ ploidyNGS_generateHistogram.R
@@ -0,0 +1,17 @@
+args = commandArgs(trailingOnly=TRUE)
+suppressMessages(library(ggplot2))
+datain<-read.table(args[1],header=F)
+colnames(datain)<-c('Chrom','Pos','Type','Freq')
+datain$Type = factor(datain$Type,
+                     levels(datain$Type)[c(1,3,4,2)])
+hist_plot<-ggplot(datain,aes(x=Freq, fill=Type)) +
+  theme_bw()+
+  geom_histogram(binwidth = 0.5, alpha=0.4) +
+  ggtitle(args[1]) +
+  ylab("Count positions") +
+  xlab("Allele Freq") +
+  scale_x_continuous(limits=c(1,100))+
+  scale_fill_manual(values=c("#920000", "#24FF24", "#6DB6FF", "#FFFF6D"))
+pdf(file=args[2])
+suppressWarnings(print(hist_plot))
+invisible(dev.off())
