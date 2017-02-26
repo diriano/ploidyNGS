d9cde2d909392eac27d78030bb32246951dea463
Diego M. Riano-Pachon <diriano@gmail.com>
2017-02-26 12:32:19 -0300

    Add license and header in simulatePloidyData.py

--- simulation/simulatePloidyData.py
+++ simulation/simulatePloidyData.py
@@ -1,5 +1,25 @@
-#!/progs/users/python-2.7.8/bin//python
+#!/usr/bin/env python 
 
+#Visually exploring ploidy with Next Generation Sequencing data
+__author__      = "Diego Mauricio Riano-Pachon & Renato Augusto Correa dos Santos"
+__copyright__   = "Copyright 2016,2017"
+__license__     = "GPL v3.0"
+__maintainer__  = "Diego Mauricio Riano-Pachon"
+__email__       = "diriano@gmail.com"
+
+"""This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 #Requires:
 #bio-python: in Ubuntu 14.04 run: sudo apt-get install python-biopython
 import argparse
